fe8deefcdbd9eff90121b0819d22fc46af27d1f9
Feihong Hsu <feihong.hsu@gmail.com>
2016-04-24 11:34:30 -0500

    Add Tabs project

---
+++ Tabs/Tabs/MyMainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Tabs
+{
+	public partial class MyMainPage : TabbedPage
+	{
+		public MyMainPage ()
+		{
+			InitializeComponent ();
+
+			ipPage.Appearing += (object sender, EventArgs e) => {
+				ipLabel.Text = "...";
+			};
+
+			luckyPage.Appearing += (object sender, EventArgs e) => {
+				Random rand = new Random();
+				luckyLabel.Text = $"{rand.Next(100)}";
+			};
+		}
+
+		public void InitProperties() {
+			dimensionsLabel.Text = $"{App.ScreenWidth} x {App.ScreenHeight}";
+		}
+	}
+}
+
---
+++ Tabs/Tabs/Tabs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+
+namespace Tabs
+{
+	public class App : Application
+	{
+		public static int ScreenWidth; 
+		public static int ScreenHeight; 
+
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new MyMainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+			MyMainPage page = (MyMainPage)this.MainPage;
+			page.InitProperties ();
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
