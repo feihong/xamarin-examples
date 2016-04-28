a9379a6ae06cfb4ba72dcdfe7475102d38b2746c
Feihong Hsu <feihong.hsu@gmail.com>
2016-04-27 22:58:52 -0500

    Added BigDemo project

---
+++ BigDemo/BigDemo/MyMainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Xamarin.Forms;
+
+namespace BigDemo
+{
+	public partial class MyMainPage : MasterDetailPage
+	{
+		public MyMainPage ()
+		{
+			InitializeComponent ();
+
+			listView.ItemsSource = Enumerable.Range (1, 10).Select (x => $"Option {x}");
+		}
+	}
+}
+
