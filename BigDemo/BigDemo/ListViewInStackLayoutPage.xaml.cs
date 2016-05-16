896f7509fd1402fccc26a53fbb1354deac1bab25
Feihong Hsu <feihong.hsu@gmail.com>
2016-05-16 13:03:24 -0500

    Add mechanism for displaying detail pages after clicking on item in master page.

---
+++ BigDemo/BigDemo/ListViewInStackLayoutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BigDemo
+{
+	public partial class ListViewInStackLayoutPage : ContentPage
+	{
+		public ListViewInStackLayoutPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
--- BigDemo/BigDemo/MyMainPage.xaml.cs
+++ BigDemo/BigDemo/MyMainPage.xaml.cs
@@ -9,22 +9,18 @@ namespace BigDemo
 	public class ListItem {
 		public string Title { get; set; }
 		public string Icon { get; set; }
+		public Type TargetType { get; set; }
 	};
 
 	public partial class MyMainPage : MasterDetailPage
 	{
 		IEnumerable<ListItem> items = new List<ListItem> {
 			new ListItem {
-				Title="Apricot",
-				Icon="lightbulb"
+				Title="ListView in StackLayout",
+				TargetType=typeof(ListViewInStackLayoutPage),
 			},
 			new ListItem {
-				Title="Banana",
-				Icon="lightbulb"
-			},
-			new ListItem {
-				Title="Cornucopia",
-				Icon="lightbulb"
+				Title="Text to speech",
 			},
 		};
 
@@ -33,10 +29,22 @@ namespace BigDemo
 			InitializeComponent ();
 
 			items = items.Select (item => {
-				item.Icon = item.Icon + ".png";
+				item.Icon = "lightbulb.png";
 				return item;
 			});			
 			listView.ItemsSource = items;
+
+			listView.ItemSelected += OnItemSelected;
+		}
+
+		void OnItemSelected(object sender, SelectedItemChangedEventArgs e) {
+			var item = e.SelectedItem as ListItem;
+
+			if (item != null && item.TargetType != null) {
+				Detail = new NavigationPage ((Page)Activator.CreateInstance(item.TargetType));
+				listView.SelectedItem = null;
+				IsPresented = false;
+			}
 		}
 	}
 }
